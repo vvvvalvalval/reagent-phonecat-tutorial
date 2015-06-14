157705c1a90e4d9ef5f2c3166e06a36168fdb25a
Valentin Waeselynck <val@bandsquare.fr>
2015-06-14 17:51:35 +0200

    first commit, generated from reagent leiningen template

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/reagent-phonecat-tutorial.jar clojure.main -m reagent-phonecat-tutorial.server
---
+++ project.clj
@@ -0,0 +1,91 @@
+(defproject reagent-phonecat-tutorial "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :source-paths ["src/clj" "src/cljs"]
+
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [ring-server "0.4.0"]
+                 [cljsjs/react "0.13.3-0"]
+                 [reagent "0.5.0"]
+                 [reagent-forms "0.5.1"]
+                 [reagent-utils "0.1.4"]
+                 [ring "1.3.2"]
+                 [ring/ring-defaults "0.1.5"]
+                 [prone "0.8.2"]
+                 [compojure "1.3.4"]
+                 [hiccup "1.0.5"]
+                 [environ "1.0.0"]
+                 [org.clojure/clojurescript "0.0-3308" :scope "provided"]
+                 [secretary "1.2.3"]]
+
+  :plugins [[lein-environ "1.0.0"]
+            [lein-asset-minifier "0.2.2"]]
+
+  :ring {:handler reagent-phonecat-tutorial.handler/app
+         :uberwar-name "reagent-phonecat-tutorial.war"}
+
+  :min-lein-version "2.5.0"
+
+  :uberjar-name "reagent-phonecat-tutorial.jar"
+
+  :main reagent-phonecat-tutorial.server
+
+  :clean-targets ^{:protect false} [[:cljsbuild :builds :app :compiler :output-dir]
+                                    [:cljsbuild :builds :app :compiler :output-to]]
+
+  :minify-assets
+  {:assets
+    {"resources/public/css/site.min.css" "resources/public/css/site.css"}}
+
+  :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
+                             :compiler {:output-to     "resources/public/js/app.js"
+                                        :output-dir    "resources/public/js/out"
+                                        :asset-path   "js/out"
+                                        :optimizations :none
+                                        :pretty-print  true}}}}
+
+  :profiles {:dev {:repl-options {:init-ns reagent-phonecat-tutorial.repl
+                                  :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+
+                   :dependencies [[ring-mock "0.1.5"]
+                                  [ring/ring-devel "1.3.2"]
+                                  [weasel "0.6.0"]
+                                  [leiningen-core "2.5.1"]
+                                  [lein-figwheel "0.3.3"]
+                                  [com.cemerick/piggieback "0.2.1"]
+                                  [org.clojure/tools.nrepl "0.2.10"]
+                                  [pjstadig/humane-test-output "0.7.0"]]
+
+                   :source-paths ["env/dev/clj"]
+                   :plugins [[lein-figwheel "0.3.3"]
+                             [lein-cljsbuild "1.0.6"]]
+
+                   :injections [(require 'pjstadig.humane-test-output)
+                                (pjstadig.humane-test-output/activate!)]
+
+                   :figwheel {:http-server-root "public"
+                              :server-port 3449
+                              :css-dirs ["resources/public/css"]
+                              :ring-handler reagent-phonecat-tutorial.handler/app}
+
+                   :env {:dev true}
+
+                   :cljsbuild {:builds {:app {:source-paths ["env/dev/cljs"]
+                                              :compiler {:main "reagent-phonecat-tutorial.dev"
+                                                         :source-map true}}
+}
+}}
+
+             :uberjar {:hooks [leiningen.cljsbuild minify-assets.plugin/hooks]
+                       :env {:production true}
+                       :aot :all
+                       :omit-source true
+                       :cljsbuild {:jar true
+                                   :builds {:app
+                                             {:source-paths ["env/prod/cljs"]
+                                              :compiler
+                                              {:optimizations :advanced
+                                               :pretty-print false}}}}}})
